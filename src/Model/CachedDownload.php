9c9710947ab5401a707f74c64ca2f51082caaecc
Nicolaas <github@sunnysideup.co.nz>
2024-05-06 13:39:03 +1200

    ENH: more BG info on data

--- src/Model/CachedDownload.php
+++ src/Model/CachedDownload.php
@@ -33,6 +33,10 @@ use Sunnysideup\Download\Api\CreateProtectedDownloadAsset;
  */
 class CachedDownload extends DataObject implements Flushable
 {
+    private static $extensions = [
+        Versioned::class . '.versioned',
+    ];
+
     protected static $assets_download_folder = '__protected_downloads';
 
     public static function file_path(string $fileName): string
